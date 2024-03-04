0268e11a9affcda024a9e0a94b648b34fb23cb6a
petrostrak <pit.trak@gmail.com>
2024-03-04 19:32:33 +0200

    Find frequency of each character

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs};
+use std::{collections::HashMap, error::Error, fs};
 
 use clap::Parser;
 
@@ -10,6 +10,18 @@ struct Args {
     filename: String,
 }
 
+fn find_frequency_of_each_char(filename: String) -> Option<HashMap<char, u64>> {
+    let mut occurrence_map: HashMap<char, u64> = HashMap::new();
+    for c in filename.chars() {
+        occurrence_map
+            .entry(c)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+    }
+
+    Some(occurrence_map)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Accept a filename as an input (return error if not found)
     // cargo run -- -f gutenberg.txt
@@ -19,6 +31,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file = fs::read_to_string(args.filename)?.parse::<String>()?;
 
     // determine the frequency of each character occurring within the text.
+    let results = find_frequency_of_each_char(file).ok_or("No occurrences")?;
+    for (k, v) in results {
+        println!("{k} : {v}")
+    }
 
     Ok(())
 }
