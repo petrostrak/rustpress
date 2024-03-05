a36ebea29eb65ff67e6b2e2add7d9995685711b4
petrostrak <pit.trak@gmail.com>
2024-03-05 22:46:25 +0200

    ADD code count, NEED FIX

--- src/tree.rs
+++ src/tree.rs
@@ -4,27 +4,33 @@ use std::cmp::Ordering;
 struct Node<T: Ord> {
     key: char,
     value: T,
+    code: String,
     left: Subtree<T>,
     right: Subtree<T>,
 }
 
 #[derive(Debug)]
-struct Subtree<T: Ord>(Option<Box<Node<T>>>);
+struct Subtree<T: Ord> {
+    node: Option<Box<Node<T>>>,
+}
 
 #[derive(Debug)]
 pub struct BinaryTree<T: Ord> {
     root: Subtree<T>,
+    code: String,
 }
 
 impl<T: Ord> BinaryTree<T> {
     pub fn new() -> Self {
         Self {
             root: Subtree::new(),
+            code: String::new(),
         }
     }
 
     pub fn insert(&mut self, key: char, value: T) {
-        self.root.insert(key, value);
+        println!("{} - {}", key, self.code);
+        self.root.insert(key, value, &mut self.code);
     }
 
     fn has(&self, value: &T) -> bool {
@@ -38,22 +44,30 @@ impl<T: Ord> BinaryTree<T> {
 
 impl<T: Ord> Subtree<T> {
     fn new() -> Self {
-        Self(None)
+        Self { node: None }
     }
 
-    fn insert(&mut self, key: char, value: T) {
-        match &mut self.0 {
-            None => self.0 = Some(Box::new(Node::new(key, value))),
+    fn insert(&mut self, key: char, value: T, code: &mut String) {
+        match &mut self.node {
+            None => self.node = Some(Box::new(Node::new(key, value, code.to_string()))),
             Some(n) => match value.cmp(&n.value) {
-                Ordering::Less => n.left.insert(key, value),
+                Ordering::Less => {
+                    code.push_str("0");
+                    n.code = code.clone();
+                    n.left.insert(key, value, &mut n.code);
+                }
                 Ordering::Equal => {}
-                Ordering::Greater => n.right.insert(key, value),
+                Ordering::Greater => {
+                    code.push_str("1");
+                    n.code = code.clone();
+                    n.right.insert(key, value, &mut n.code);
+                }
             },
         }
     }
 
     fn has(&self, value: &T) -> bool {
-        match &self.0 {
+        match &self.node {
             None => false,
             Some(n) => match value.cmp(&n.value) {
                 Ordering::Less => n.left.has(value),
@@ -64,7 +78,7 @@ impl<T: Ord> Subtree<T> {
     }
 
     fn len(&self) -> usize {
-        match &self.0 {
+        match &self.node {
             None => 0,
             Some(n) => 1 + n.left.len() + n.left.len() + n.right.len(),
         }
@@ -72,10 +86,11 @@ impl<T: Ord> Subtree<T> {
 }
 
 impl<T: Ord> Node<T> {
-    fn new(key: char, value: T) -> Self {
+    fn new(key: char, value: T, code: String) -> Self {
         Self {
             key,
             value,
+            code,
             left: Subtree::new(),
             right: Subtree::new(),
         }
